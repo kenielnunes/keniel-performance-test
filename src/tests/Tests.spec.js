2549388c9d22071149a512274fe029d9266120fe
Keniel Nunes <kenielalves3@gmail.com>
2024-11-28 20:09:42 -0300

    feat: config

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -9,13 +9,20 @@ export const RateContentOK = new Rate('content_OK');
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
+    http_req_failed: ['rate<0.12'],
+    http_req_duration: ['p(95)<5700'],
     content_OK: ['rate>0.95']
   },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
+    { duration: '10s', target: 10 },
+    { duration: '30s', target: 20 },
+    { duration: '30s', target: 50 },
+    { duration: '30s', target: 70 },
+    { duration: '30s', target: 100 },
+    { duration: '40s', target: 150 },
+    { duration: '40s', target: 200 },
+    { duration: '40s', target: 250 },
+    { duration: '50s', target: 300 }
   ]
 };
 
@@ -46,6 +53,6 @@ export default function () {
   RateContentOK.add(res.status === OK);
 
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'GET - Status 200': () => res.status === OK
   });
 }
